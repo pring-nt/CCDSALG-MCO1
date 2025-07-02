72dc478ce97c409be432bae916c18ab3c912cab5
nathan <natetrndd@gmail.com>
2025-07-02 19:42:42 +0800

    fixed indent size

--- Stack.c
+++ Stack.c
@@ -1,58 +1,60 @@
 #include "Stack.h"
 
-void createStack(Stack** pStack) {
-        *pStack = NULL;
+void createStack(Stack **pStack) {
+    *pStack = NULL;
 }
 
-void push(Stack** pStack, Points value) {
-        Stack* pNew = (Stack*) malloc(sizeof(Stack));
-        if (pNew == NULL) {
-                fprintf(stderr, "Memory allocation failed\n");
-                exit(1);
-        }
+void push(Stack **pStack, Points value) {
+    Stack *pNew = (Stack *)malloc(sizeof(Stack));
+    if (pNew == NULL)
+    {
+        fprintf(stderr, "Memory allocation failed\n");
+        exit(1);
+    }
 
-        pNew->element = value;
-        pNew->pNext = *pStack;
-        *pStack = pNew;
+    pNew->element = value;
+    pNew->pNext = *pStack;
+    *pStack = pNew;
 }
 
-Points pop(Stack** pStack) {
-        if (*pStack == NULL) {
-                fprintf(stderr, "Stack Underflow!\n");
-                exit(1);
-        }
-        Stack* pTemp = *pStack;
+Points pop(Stack **pStack) {
+    if (*pStack == NULL)
+    {
+        fprintf(stderr, "Stack Underflow!\n");
+        exit(1);
+    }
+    Stack *pTemp = *pStack;
 
-        Points value = pTemp->element;
-        *pStack = pTemp->pNext;
-        free(pTemp);
+    Points value = pTemp->element;
+    *pStack = pTemp->pNext;
+    free(pTemp);
 
-        return value;
+    return value;
 }
 
-Points top(Stack* pStack) {
-        if (pStack == NULL) {
-                fprintf(stderr, "Stack is empty!\n");
-                exit(1);
-        }
+Points top(Stack *pStack) {
+    if (pStack == NULL)
+    {
+        fprintf(stderr, "Stack is empty!\n");
+        exit(1);
+    }
 
-        Points value = pStack->element;
-        return value;
+    Points value = pStack->element;
+    return value;
 }
 
-Points next_to_top(Stack* pStack) {
-        if (pStack == NULL || pStack->pNext == NULL) {
-                fprintf(stderr, "Stack doesn't have enough elements!\n");
-                exit(1);
-        }
+Points next_to_top(Stack *pStack) {
+    if (pStack == NULL || pStack->pNext == NULL)
+    {
+        fprintf(stderr, "Stack doesn't have enough elements!\n");
+        exit(1);
+    }
 
-        Stack* pTemp = pStack->pNext;
-        Points value = pTemp->element;
-        return value;
+    Stack *pTemp = pStack->pNext;
+    Points value = pTemp->element;
+    return value;
 }
 
-bool isEmpty(Stack* pStack) {
-        return pStack == NULL;
+bool isEmpty(Stack *pStack) {
+    return pStack == NULL;
 }
-
-
--- Stack.h
+++ Stack.h
@@ -3,22 +3,22 @@
 #include <stdbool.h>
 
 struct _Points {
-        double x_pos; 
-        double y_pos;
+    double x_pos;
+    double y_pos;
 };
 
 typedef struct _Points Points;
 
 struct _Stack {
-        Points element;
-        struct _Stack* pNext;
+    Points element;
+    struct _Stack *pNext;
 };
 
 typedef struct _Stack Stack;
 
-void createStack(Stack**);
-void push(Stack**, Points);
-Points pop(Stack**);
-Points top(Stack*);
-Points next_to_top(Stack*);
-bool isEmpty(Stack*);
+void createStack(Stack **);
+void push(Stack **, Points);
+Points pop(Stack **);
+Points top(Stack *);
+Points next_to_top(Stack *);
+bool isEmpty(Stack *);
