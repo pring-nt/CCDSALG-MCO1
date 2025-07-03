540de1303f6f5883375a0d64f006ee55544482db
Chelsea Jei R. Dimaunahan <chelsea_dimaunahan@dlsu.edu.ph>
2025-07-04 02:21:05 +0800

    Update sort.h
    added the math.h lib & the declaration for the computePolarAngles function,, also a member of a struct for the polar angles

--- sort.h
+++ sort.h
@@ -1,15 +1,24 @@
+/*
+GROUP NUMBER : 45
+    LASTNAME1, FIRSTNAME1: TRINIDAD, NATHAN         SECTION1: S13
+    LASTNAME2, FIRSTNAME2: DIMAUNAHAN, CHELSEA JEI  SECTION2: S14
+    LASTNAME3, FIRSTNAME3: SALAMIDA, GABRIEL        SECTION3: S13
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 struct _Points {
     double x;
     double y;
+    double polarAngle;
 };
 
 typedef struct _Points Points;
 
 void findAnchor(Points points[], int n);
 double crossProduct(Points a, Points b, Points anchor);
+void computePolarAngles(Points points[], int n, Points anchor);
 double distanceSquared(Points a, Points anchor);
-int comparePolar(Points a, Points b, Points anchor);
 void selectionSort(Points points[], int n);
